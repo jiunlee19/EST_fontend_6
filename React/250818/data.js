c7dc14fb2503587a1262069c19a2e2735b69fa60
jiunlee19 <tearofglass@naver.com>
2025-08-19 23:56:16 +0900

    250818 수업 내용 추가

---
+++ React/250818/data.js
@@ -0,0 +1,23 @@
+export const accommodationsData = [
+    {
+        id: 1,
+        type: "도심 아파트",
+        location: "강남구",
+        price: 85000,
+        amenities: ["무료 주차 공간", "주방", "TV"],
+    },
+    {
+        id: 2,
+        type: "럭셔리 펜트하우스",
+        location: "한남동",
+        price: 250000,
+        amenities: ["대형 욕조", "수영장", "게스트용 별채", "주방", "TV"],
+    },
+    {
+        id: 3,
+        type: "산속 별장",
+        location: "평창",
+        price: 95000,
+        amenities: ["바베큐 그릴", "게스트용 별채", "무료 주차 공간", "TV"],
+    },
+];
